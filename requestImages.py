95524927dbf0d09b07a7910c425a917c564cbaca
Sohum Sharma <102423861+sohumsharma484@users.noreply.github.com>
2023-02-10 12:32:48 -0600

    Update requestImages.py

--- requestImages.py
+++ requestImages.py
@@ -1,24 +1,43 @@
 import requests
 import shutil
 from bs4 import BeautifulSoup
+import os
 # pip install lxml
 
-tissueSubBlock = "W6-1-1-F.2.02"
-# Get ID For Image
-getID = requests.get(f"http://api.brain-map.org/api/v2/data/query.xml?criteria=model::SectionDataSet,rma::criteria,specimen[external_specimen_name$eq'{tissueSubBlock}'],treatments[name$eq'ISH'],rma::include,genes,sub_images", stream=True)
-if getID.status_code == 200:
-    Bs_data = BeautifulSoup(getID.text, "xml")
-    ID = Bs_data.find("sub-image").find("id").text
+def requestImage(tissueSubBlock):
+    file_dir = os.path.dirname(os.path.realpath('__file__'))
+    os.mkdir(f"{tissueSubBlock}")
 
-    # Download Image
-    img = requests.get(f"http://api.brain-map.org/api/v2/image_download/{ID}?view=tumor_feature_annotation",
-                       stream=True)
-    if img.status_code == 200:
-        with open("image.jpg", 'wb') as f:
-            img.raw.decode_content = True
-            shutil.copyfileobj(img.raw, f)
-    else:
-        print(img.status_code)
-else:
-    print(getID.status_code)
+    # Get ID For Sub Block
+    getID = requests.get(
+        f"http://api.brain-map.org/api/v2/data/query.xml?criteria=model::SectionDataSet,rma::criteria,specimen[external_specimen_name$eq'{tissueSubBlock}'],treatments[name$eq'ISH'],rma::include,genes,sub_images",
+        stream=True)
+    if getID.status_code == 200:
+        Bs_data = BeautifulSoup(getID.text, "xml")
+        images = Bs_data.findAll("section-data-set")
+        for image in images:
+            gene = image.findNext("acronym").text
+            ID = image.find("sub-image").find("id").text
+            print(ID)
+            print(gene)
 
+            # Download ISH Image
+            img = requests.get(f"http://api.brain-map.org/api/v2/image_download/{ID}", stream=True)
+            if img.status_code == 200:
+                with open(f"{tissueSubBlock}/{tissueSubBlock}_{ID}_{gene}_ISH.jpg", 'wb') as f:
+                    img.raw.decode_content = True
+                    shutil.copyfileobj(img.raw, f)
+            else:
+                print(img.status_code)
+
+            # Download corresponding annotated image
+            img = requests.get(f"http://api.brain-map.org/api/v2/image_download/{ID}?view=tumor_feature_annotation",
+                               stream=True)
+            if img.status_code == 200:
+                with open(f"{tissueSubBlock}/{tissueSubBlock}_{ID}_{gene}_annotated.jpg", 'wb') as f:
+                    img.raw.decode_content = True
+                    shutil.copyfileobj(img.raw, f)
+            else:
+                print(img.status_code)
+
+    return
