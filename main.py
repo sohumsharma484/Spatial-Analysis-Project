543d8262c04eeb308ed4ab989aff92ace25273bd
Sohum Sharma <102423861+sohumsharma484@users.noreply.github.com>
2023-02-28 09:42:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from detectBoundaries import detectBoundaries
 import os
 from threading import Thread
 
-
 # Variables
-tissueSubBlock = "W27-2-1-I.02"
+tissueSubBlock = "W31-1-1-E.01"
 QuPathAbsPath = r"C:\Users\sohum\Downloads\QuPath-0.4.2-Windows\QuPath-0.4.2\QuPath-0.4.2 (console).exe"
 RscriptAbsPath = r'C:\"Program Files"\R\R-4.2.2\bin\Rscript.exe'
 currentDirectory = r"C:\Users\sohum\OneDrive\Documents\MDAnderson"
@@ -21,11 +20,8 @@ for file in os.listdir(tissueSubBlock):
     if "annotated.jpg" in filename:
         imagePath = os.path.join(tissueSubBlock, filename)
         detectBoundaries(imagePath)
-        print(imagePath)
         t = Thread(target=detectBoundaries, args=(imagePath,))
         threads.append(t)
-        # thread = Thread(target=detectBoundaries, args=(imagePath,))
-        # thread.start()
 
 # Start all threads
 for x in threads:
@@ -34,13 +30,14 @@ for x in threads:
 # Wait for all of them to finish
 for x in threads:
     x.join()
+    
+print("boundary detection complete")
 
 # Detect Cells
 for file in os.listdir(tissueSubBlock):
     filename = os.fsdecode(file)
     if "ISH" in filename:
         imagePath = os.path.join(tissueSubBlock, filename)
-        print(imagePath)
         os.system(f'cmd /c ""{QuPathAbsPath}" script --image={os.path.abspath(imagePath)} "{os.path.abspath("detectCells.groovy")}"')
 
 # Load into Spatstat
@@ -55,6 +52,5 @@ for file in os.listdir(tissueSubBlock):
 
         command = rf'cmd /c {RscriptAbsPath} "{currentDirectory}\mapCellsToRegions.R" "{currentDirectory}\{tissueSubBlock}\{filename}" "{currentDirectory}\{tissueSubBlock}\{parts[0]}_{parts[1]}_{parts[2]}_ISH_detections.csv" "{currentDirectory}\{tissueSubBlock}\{parts[0]}_{parts[1]}_{parts[2]}.Rdata"'
 
-        # runR(command)
         thread = Thread(target=runR, args=(command,))
         thread.start()
