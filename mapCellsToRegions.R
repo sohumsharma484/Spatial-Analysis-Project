6ca9b9fb005e2278412a753c60d11b03c0379da6
Sohum Sharma <102423861+sohumsharma484@users.noreply.github.com>
2023-02-28 09:12:41 -0600

    Update mapCellsToRegions.R

--- mapCellsToRegions.R
+++ mapCellsToRegions.R
@@ -1,68 +1,76 @@
 library (spatstat)
 library("rlist")
 
-boundarysData = read.csv("sampleOutput.csv")
-pointsData = read.csv("detections.csv", sep="\t")
+
+options(echo=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
+boundaryDataPath = args[1]
+pointsDataPath = args[2]
+destinationRdataPath = args[3]
 
 
-boundaryXVector = c()
-boundaryYVector = c()
-boundaryXList = list()
-boundaryYList = list()
-boundaryList = list()
-shapeID = 0
-polyID = 0
-# Must be "Black", "Light Blue", "Blue", "Green", "Pink"
-SHAPECOLOR = "Light Blue"
+boundarysData = read.csv(boundaryDataPath)
+pointsData = read.csv(pointsDataPath, sep="\t")
 
-for(i in 1:nrow(boundarysData)) {
 
-  if (boundarysData[i, 5] != SHAPECOLOR)
-  {
+pointXList = pointsData[ , "Centroid.X.px"]
+pointYList = pointsData[ , "Centroid.Y.px"]
+
+
+pointPatterns = list()
+regions = c("Infiltrating Tumor", "Perinecrotic zone", "Leading Edge", "Pseudopalisading cells but no visible necrosis", "Cellular Tumor", "Necrosis", "Microvascular proliferation", "Hyperplastic blood vessels", "Pseudopalisading cells around necrosis")
+for (region in regions) {
+  shapeID = 0
+  boundaryXVector <<- c()
+  boundaryYVector <<- c()
+  boundaryList <<- list()
+  for(i in 1:nrow(boundarysData)) {
+    
+    if (boundarysData[i, 4] != region)
+    {
+      next
+    }
+    
+    if (boundarysData[i, 3] > shapeID)
+    {
+      if (length(boundaryXVector) > 2) {
+        # Reverse to avoid negative area
+        boundaryList <<- list.append(boundaryList, list(x=rev(boundaryXVector), y=rev(boundaryYVector)))
+        
+      }
+
+      boundaryXVector <<- c()
+      boundaryYVector <<- c()
+      shapeID = shapeID + 1
+    }
+    
+    boundaryXVector <<- append(boundaryXVector, boundarysData[i, 1])
+    boundaryYVector <<- append(boundaryYVector, boundarysData[i, 2])
+  }
+  
+  if (length(boundaryXVector) < 3 && length(boundaryList) == 0){
     next
   }
 
-  if (boundarysData[i, 4] > polyID)
-  {
-    polyID = polyID + 1
-    shapeID = 0
-  }
-
-
-  if (boundarysData[i, 3] > shapeID)
-  {
-    boundaryXList = append(boundaryXList, boundaryXVector)
-    boundaryYList = append(boundaryYList, boundaryYVector)
+  if (length(boundaryXVector) > 2) {
     # Reverse to avoid negative area
-    boundaryList = list.append(boundaryList, list(x=rev(boundaryXVector), y=rev(boundaryYVector)))
-
-    boundaryXVector = c()
-    boundaryYVector = c()
-    shapeID = shapeID + 1
+    boundaryList <<- list.append(boundaryList, list(x=rev(boundaryXVector), y=rev(boundaryYVector)))
   }
-
   
-  boundaryXVector = append(boundaryXVector, boundarysData[i, 1])
-  boundaryYVector = append(boundaryYVector, boundarysData[i, 2])
+  tryCatch(
+    #try to do this
+    {
+      pp <- ppp(x=pointXList, y=pointYList, poly=boundaryList)
+      marks(pp) <- rep(region, npoints(pp))
+      pp <- as.ppp(pp)
+      pointPatterns = append(pointPatterns, list(pp))
+    },
+    error=function(e) 
+    {
+      print(e)
+    }
+  )
 }
 
 
-pointXList = c()
-pointYList = c()
-for(i in 1:nrow(pointsData)) {       # for-loop over rows
-  pointXList = append(pointXList, pointsData[i, 1])
-  pointYList = append(pointYList, pointsData[i, 2])
-}
-
-
-boundaryList
-
-pp <- ppp(x=pointXList, y=pointYList, poly=boundaryList)
-W <- Window(pp)
-pp <- as.ppp(pp)
-# reverse y axis to correct polygonal orientation
-# Plot boundaries
-plot(W, ylim=rev(W$yrange), main=NULL)
-# Add cell points
-plot(pp, add=T)
-
+save(pointPatterns, file=destinationRdataPath, compress=F)
