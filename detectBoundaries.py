1dd9a4f7e03dd225c95ba18b14bc334672d80b01
Sohum Sharma <102423861+sohumsharma484@users.noreply.github.com>
2023-02-28 08:28:11 -0600

    Update detectBoundaries.py

--- detectBoundaries.py
+++ detectBoundaries.py
@@ -1,71 +1,59 @@
 import cv2
 import numpy as np
 import pandas as pd
-import sqlite3 as sl
-
-IMAGEPATH = ""
-image = cv2.imread(IMAGEPATH)
-original = image.copy()
-image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-con = sl.connect('system.db')
-# Create coordinate database
-studentList = pd.DataFrame({
-    'x': [],
-    'y': [],
-    'shapeID': [],
-    'colorNum': [],
-    'color': [],
-})
-studentList.to_sql("coordinates", con, index= False)
-
-# Retrieve database values
-c = con.cursor()
-c.execute('''
-          SELECT
-          *
-          FROM coordinates
-          ''')
-coordinatesDb = pd.DataFrame(c.fetchall(), columns=['x', 'y', 'shapeID', 'colorNum', 'classification'])
 
 
-colorOptions = [
-    ["Black", np.array([0, 0, 0], dtype="uint8"), np.array([10, 10, 10], dtype="uint8")],
-    ["Light Blue", np.array([96, 100, 210], dtype="uint8"), np.array([127, 255, 255], dtype="uint8")],
-    ["Blue", np.array([90, 100, 20], dtype="uint8"), np.array([95, 255, 200], dtype="uint8")],
-    ["Green", np.array([40, 100, 20], dtype="uint8"), np.array([80, 255, 255], dtype="uint8")],
-    ["Pink", np.array([140, 100, 20], dtype="uint8"), np.array([160, 255, 255], dtype="uint8")],
-    ['Red', np.array([0, 50, 20], dtype="uint8"), np.array([12, 255, 255], dtype="uint8")]
-]
+def detectBoundaries(imagePath):
+    image = cv2.imread(imagePath)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-colorNum = 0
-codes = []
-for colorOption in colorOptions:
-    mask = cv2.inRange(image, colorOption[1], colorOption[2])
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    polyNum = 0
+    # Create coordinate dataframe
+    coordinatesDb = pd.DataFrame({
+        'x': [],
+        'y': [],
+        'shapeID': [],
+        'color': [],
+    })
 
-    for contour in contours:
-        approx = cv2.approxPolyDP(contour, 0.00009 * cv2.arcLength(contour, True), True)
-        area = cv2.contourArea(contour)
-        if area < 1000 or area > 200000000:
-            continue
 
-        # Used to flatten the array containing the co-ordinates of the vertices.
-        n = approx.ravel()
+    colorOptions = [
+        ["Infiltrating Tumor", np.array([148, 100, 20], dtype="uint8"), np.array([152, 255, 255], dtype="uint8")],
+        ["Perinecrotic zone", np.array([96, 100, 210], dtype="uint8"), np.array([98, 255, 255], dtype="uint8")],
+        ["Leading Edge", np.array([94, 100, 20], dtype="uint8"), np.array([95, 255, 200], dtype="uint8")],
+        ["Pseudopalisading cells but no visible necrosis", np.array([103, 100, 20], dtype="uint8"), np.array([135, 255, 255], dtype="uint8")],
+        ["Cellular Tumor", np.array([60, 100, 20], dtype="uint8"), np.array([62, 255, 255], dtype="uint8")],
+        ['Necrosis', np.array([0, 0, 0], dtype="uint8"), np.array([10, 10, 10], dtype="uint8")],
+        ['Microvascular proliferation', np.array([4, 50, 20], dtype="uint8"), np.array([6, 255, 255], dtype="uint8")],
+        ['Hyperplastic blood vessels', np.array([11, 50, 20], dtype="uint8"), np.array([12, 255, 255], dtype="uint8")],
+        ['Pseudopalisading cells around necrosis', np.array([83, 50, 20], dtype="uint8"), np.array([85, 255, 255], dtype="uint8")]
+    ]
 
-        i = 0
-        for j in n:
-            if (i % 2 == 0):
-                x = n[i]
-                y = n[i + 1]
-                row = {"x": x, "y": y, "shapeID": int(polyNum), "colorNum": int(colorNum), "color": colorOption[0]}
-                coordinatesDb = coordinatesDb.append(row, ignore_index=True)
-            i += 1
+    for colorOption in colorOptions:
+        mask = cv2.inRange(image, colorOption[1], colorOption[2])
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        polyNum = 0
 
-        polyNum += 1
+        for contour in contours:
+            approx = cv2.approxPolyDP(contour, 0.00009 * cv2.arcLength(contour, True), True)
+            area = cv2.contourArea(contour)
 
-    colorNum += 1
+            if area < 1000:
+                continue
 
-coordinatesDb.to_csv("output.csv", index=False)
-coordinatesDb.to_sql('coordinates', con, index=False, if_exists='replace')
+            # Used to flatten the array containing the co-ordinates of the vertices.
+            n = approx.ravel()
+
+            i = 0
+            for j in n:
+                if (i % 2 == 0):
+                    x = n[i]
+                    y = n[i + 1]
+                    row = {"x": x, "y": y, "shapeID": polyNum, "color": colorOption[0]}
+                    coordinatesDb = coordinatesDb.append(row, ignore_index=True)
+                i += 1
+
+            polyNum += 1
+        cv2.imwrite("mask.jpg", mask)
+
+
+    coordinatesDb.to_csv(f"{imagePath[:-4]}_boundaries.csv", index=False)
